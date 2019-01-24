99dc1bc9389d4c9b1f1fa86723ec96ffc4769ea7
Sydney Stubbs <sydneytstubbs@gmail.com>
2019-01-24 11:23:51 -0800

    Hero Update

--- js/portfolio.js
+++ js/portfolio.js
@@ -1,11 +1,16 @@
-script.js
-function openSlideMenu(){
- document.getElementById('side-menu').style.width = '250px';
- document.getElementById('main').style.marginLeft = '250px';	document.getElementById('ham').style.visibility = "hidden";
-}
+// envelope animation
+window.onload = function(){
+  var tl = new TimelineLite({delay: 1}),
+    firstBg = document.querySelectorAll('.text__first-bg'),
+    secBg = document.querySelectorAll('.text__second-bg'),
+    word  = document.querySelectorAll('.text__word');
 
-function closeSlideMenu(){
- document.getElementById('side-menu').style.width = '0';
- document.getElementById('ham').style.visibility = "visible";
- document.getElementById('main').style.marginLeft = '-50px'
+  tl
+    .to(firstBg, 0.2, {scaleX:1})
+    .to(secBg, 0.2, {scaleX:1})
+    .to(word, 0.1, {opacity:1}, "-=0.1")
+    .to(firstBg, 0.2, {scaleX:0})
+    .to(secBg, 0.2, {scaleX:0});
+
+  document.querySelector('.restart').onclick = function() {tl.restart()};
 }
