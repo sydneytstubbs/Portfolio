7579715ddfc41e3dfdc0627bd6f346b90602f35f
Sydney Stubbs <sydneytstubbs@gmail.com>
2019-02-15 11:43:07 -0800

    Hero Change

--- js/portfolio.js
+++ js/portfolio.js
@@ -1,19 +1,24 @@
-// envelope animation
-window.onload = function(){
-  var tl = new TimelineLite({delay: 1}),
-    firstBg = document.querySelectorAll('.text__first-bg'),
-    secBg = document.querySelectorAll('.text__second-bg'),
-    thirdBg = document.querySelectorAll('.text__third-bg'),
-    word  = document.querySelectorAll('.text__word');
-
-  tl
-    .to(firstBg, 0.2, {scaleX:1})
-    .to(secBg, 0.2, {scaleX:1})
-    .to(thirdBg, 0.2,{scaleX:1})
-    .to(word, 0.1, {opacity:1}, "-=0.1")
-    .to(firstBg, 0.2, {scaleX:0})
-    .to(secBg, 0.2, {scaleX:0})
-    .to(thirdBg, 0.2,{scaleX:0});
-
-  document.querySelector('.restart').onclick = function() {tl.restart()};
-}
+/*-----
+Spanizer
+- Wraps letters with spans, for css animations
+-----*/
+(function($) {
+  var s,
+  spanizeLetters = {
+    settings: {
+      letters: $('.js-spanize'),
+    },
+    init: function() {
+      s = this.settings;
+      this.bindEvents();
+    },
+    bindEvents: function(){
+      s.letters.html(function (i, el) {
+        //spanizeLetters.joinChars();
+        var spanizer = $.trim(el).split("");
+        return '<span>' + spanizer.join('</span><span>') + '</span>';
+      });
+    },
+  };
+  spanizeLetters.init();
+})(jQuery);
