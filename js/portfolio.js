81986bfd3422116b8f8788204b37cd822fb3b77d
Sydney Stubbs <sydneytstubbs@gmail.com>
2019-02-14 16:09:00 -0800

    JS Update

--- js/portfolio.js
+++ js/portfolio.js
@@ -3,14 +3,17 @@ window.onload = function(){
   var tl = new TimelineLite({delay: 1}),
     firstBg = document.querySelectorAll('.text__first-bg'),
     secBg = document.querySelectorAll('.text__second-bg'),
+    thirdBg = document.querySelectorAll('.text__third-bg'),
     word  = document.querySelectorAll('.text__word');
 
   tl
     .to(firstBg, 0.2, {scaleX:1})
     .to(secBg, 0.2, {scaleX:1})
+    .to(thirdBg, 0.2,{scaleX:1})
     .to(word, 0.1, {opacity:1}, "-=0.1")
     .to(firstBg, 0.2, {scaleX:0})
-    .to(secBg, 0.2, {scaleX:0});
+    .to(secBg, 0.2, {scaleX:0})
+    .to(thirdBg, 0.2,{scaleX:0});
 
   document.querySelector('.restart').onclick = function() {tl.restart()};
 }
